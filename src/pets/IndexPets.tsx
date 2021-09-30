b9c47d4aab4eb6c82b8103c1f8aef54027917908
Catalina <moreracata@gmail.com>
2021-09-30 13:28:24 -0600

    Add more option

--- src/pets/IndexPets.tsx
+++ src/pets/IndexPets.tsx
@@ -4,16 +4,28 @@ import cat from '../img/gato.png';
 import collar from '../img/collar.png';
 import rascador from '../img/rascador.png';
 import huella from '../img/huella.png';
+import pez from '../img/pez.png';
+import mouse2 from '../img/mouse2.png';
+import mouse from '../img/mouse.png';
+import gato2 from '../img/gato2.png';
+
 
 import { landingOptionDTO } from "./pet.model";
 
 
 export default function IndexPets(){
     const options: landingOptionDTO[] = [
-        { id:1, image: cat, tag: 'NEW PET', url: '/newpet' }, 
-        { id:1, image: huella, tag: 'NEW OWNER' }, 
-        { id:2,image: collar, tag: 'OWNER DETAILS' },
-        { id:3,image: rascador, tag: 'PET DETAILS' }
+        { id:1, image: cat, tag: 'REGISTER A PET', url: '/newpet' }, 
+        { id:1, image: huella, tag: 'REGISTER A OWNER' }, 
+        { id:2,image: collar, tag: 'EDIT OWNER DETAILS' },
+        { id:3,image: rascador, tag: 'EDIT PET DETAILS' }
+    ]
+
+    const options2: landingOptionDTO[] = [
+        { id:1, image: pez, tag: 'OPTION', url: '/newpet' }, 
+        { id:1, image: mouse, tag: 'OPTION' }, 
+        { id:2,image: mouse2, tag: 'OPTION' },
+        { id:3,image: gato2, tag: 'OPTION' }
     ]
 
 
@@ -23,6 +35,10 @@ export default function IndexPets(){
             <h1>Pets Options </h1>
             <div className="row ">   
                 {options.map(option =>
+                <IndexOption option={option} key={option.id} />)}
+            </div>  
+            <div className="row ">   
+                {options2.map(option =>
                 <IndexOption option={option} key={option.id} />)}
             </div>  
             </div>   
