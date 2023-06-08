335c9b01d7ff7a773dfafeccca1edc9b3f242dc9
Catalina Morera <cmorera@rms.com>
2023-06-07 23:26:23 -0700

    Remove hardcoded data from carousel

--- src/utils/Carousel/Carousel.tsx
+++ src/utils/Carousel/Carousel.tsx
@@ -13,35 +13,14 @@ export default function Carousel(props: carouselProps){
                 { id:2, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193260.png', tag: 'OPCION2' }, 
                 { id:3, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193312.png', tag: 'OPCION3' }
             ]
-        );
-
-
-    const options: CarouselOptionDTO[] = [
-        { id:0, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193375.png', tag: 'OPCION0'},
-        { id:1, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193256.png', tag: 'OPCION1'}, 
-        { id:2, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193260.png', tag: 'OPCION2' }, 
-        { id:3, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193312.png', tag: 'OPCION3' },
-        { id:4, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193308.png', tag: 'OPCION4'}, 
-        { id:5, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193288.png', tag: 'OPCION5' }, 
-        { id:6, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193354.png', tag: 'OPCION6' },
-        { id:7, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193310.png', tag: 'OPCION7'}, 
-        { id:8, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193251.png', tag: 'OPCION8' }, 
-        { id:9, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193243.png', tag: 'OPCION9' },
-        { id:10, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193285.png', tag: 'OPCION10'}, 
-        { id:11, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193351.png', tag: 'OPCION11' }, 
-        { id:12, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193347.png', tag: 'OPCION12' },
-        
-    ]
-
-    useEffect(() => {
-    });
-
+    )
+    
     const moveToLeft = () => {
         let left = current[0].id
         let right = current[2].id
         
             if( current[0].id != 0){
-           var newCurrent = [options[right-3],options[right-2],options[right-1]]
+           var newCurrent = [props.options[right-3],props.options[right-2],props.options[right-1]]
            setCurrent(newCurrent)
         }
     }
@@ -51,8 +30,8 @@ export default function Carousel(props: carouselProps){
         let left = current[0].id
         let right = current[2].id
        
-        if(right != options.length-1){
-            var newCurrent = [options[left+1],options[left+2],options[left+3]]
+        if(right != props.options.length-1){
+            var newCurrent = [props.options[left+1],props.options[left+2],props.options[left+3]]
            setCurrent(newCurrent)
         }
     }
@@ -79,11 +58,10 @@ export default function Carousel(props: carouselProps){
                 </div>
             </div>   
         </>
-    )
-        
+    )       
 }
  
 
 interface carouselProps{
-    options?: CarouselOptionDTO[];
+    options: CarouselOptionDTO[];
 }
